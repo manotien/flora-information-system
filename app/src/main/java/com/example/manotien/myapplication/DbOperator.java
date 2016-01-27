38690dd773f84c42b05362f5220886bef3cf7263
manotien <manotien@gmail.com>
2016-01-27 20:26:22 +0700

    init project

---
+++ app/src/main/java/com/example/manotien/myapplication/DbOperator.java
@@ -0,0 +1,35 @@
+package com.example.manotien.myapplication;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DbOperator extends SQLiteOpenHelper{
+
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "DB_NAME";
+    protected static final String FIRST_TABLE_NAME = "FIRST_TABLE";
+
+    public static final String CREATE_FIRST_TABLE = "create table if not exists "
+            + FIRST_TABLE_NAME
+            + " ( _id integer primary key autoincrement, province  TEXT NOT NULL, district TEXT NOT NULL,subdistrict TEXT NOT NULL"
+            + ");";
+
+
+    public DbOperator(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_FIRST_TABLE);
+        //db.close();
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        //THIS WILL BE EXECUTED WHEN YOU UPDATED VERSION OF DATABASE_VERSION
+        //YOUR DROP AND CREATE QUERIES
+    }
+
+}
