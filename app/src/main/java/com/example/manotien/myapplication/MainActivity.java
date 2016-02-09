c5f76626a812e3154d74dbd791c672b6a8a363ec
manotien <manotien@gmail.com>
2016-02-09 21:31:29 +0700

    export csv

--- app/src/main/java/com/example/manotien/myapplication/MainActivity.java
+++ app/src/main/java/com/example/manotien/myapplication/MainActivity.java
@@ -1,15 +1,26 @@
 package com.example.manotien.myapplication;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.content.Intent;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.opencsv.CSVWriter;
+
+import java.io.File;
+import java.io.FileWriter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,7 +47,46 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        Button button1 = (Button)findViewById(R.id.export);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                File dbFile=getDatabasePath("FLORA");
+                DbOperator dbhelper = new DbOperator(getApplicationContext());
+                File exportDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS), "Flora");
+                Log.d(exportDir.toString(),"kuy");
+                if (!exportDir.exists())
+                {
+                    exportDir.mkdirs();
+
+                }
+
+                File file = new File(exportDir, "csvname.csv");
+
+                try
+                {
+                    file.createNewFile();
+                    CSVWriter csvWrite = new CSVWriter(new FileWriter(file));
+                    SQLiteDatabase db = dbhelper.getReadableDatabase();
+                    Cursor curCSV = db.rawQuery("SELECT * FROM FIRST_TABLE",null);
+                    csvWrite.writeNext(curCSV.getColumnNames());
+                    while(curCSV.moveToNext())
+                    {
+                        //Which column you want to exprort
+                        String arrStr[] ={curCSV.getString(0),curCSV.getString(1), curCSV.getString(2)};
+                        csvWrite.writeNext(arrStr);
+                    }
+                    csvWrite.close();
+                    curCSV.close();
+                    Toast.makeText(getApplicationContext(), "Export Success", Toast.LENGTH_LONG).show();
+                }
+                catch(Exception sqlEx)
+                {
+                    Log.e("MainActivity", sqlEx.getMessage(), sqlEx);
+                }
+            }
+        });
     }
 
     @Override
