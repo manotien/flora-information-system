3ac2456b795f3a6a243d3f5896ce156fbf8aa8ad
manotien <manotien@gmail.com>
2016-02-23 00:27:06 +0700

    Add Exploration Page UI

--- app/src/main/java/com/example/manotien/myapplication/Main2Activity.java
+++ app/src/main/java/com/example/manotien/myapplication/Main2Activity.java
@@ -23,37 +23,29 @@ public class Main2Activity extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        setTitle("Start");
+        setTitle("Exploration Place");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-
-        String[] province_item = new String[]{"Bangkok", "Chumphon", "Chonburi"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, province_item);
+        String[] protect_item = new String[]{"Test", "Yoyo", "AAAA"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, protect_item);
         Spinner province = (Spinner)findViewById(R.id.spinner);
         province.setAdapter(adapter);
 
 
-        Button button = (Button)findViewById(R.id.button3);
+        Button button = (Button)findViewById(R.id.nextbut);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Spinner province = (Spinner) findViewById(R.id.spinner);
-                EditText place = (EditText) findViewById(R.id.place);
+                //Spinner province = (Spinner) findViewById(R.id.spinner);
+                //EditText place = (EditText) findViewById(R.id.place);
 
                 Intent intent = new Intent(Main2Activity.this, Survey_Main.class);
-                intent.putExtra("ans_province", province.getSelectedItem().toString());
-                intent.putExtra("ans_place", place.getText().toString());
+                //intent.putExtra("ans_province", province.getSelectedItem().toString());
+                //intent.putExtra("ans_place", place.getText().toString());
                 startActivity(intent);
             }
         });
 
-        Button button1 = (Button)findViewById(R.id.button4);
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Main2Activity.this, MainMenuActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 
 }
