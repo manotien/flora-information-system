d8792d75fc5e7b1a0b3cd8cd1445a0e849d235ae
manotien <manotien@gmail.com>
2016-04-11 15:16:18 +0700

    add export data intent Co-Collector can select

--- app/src/main/java/com/example/manotien/myapplication/Main2Activity.java
+++ app/src/main/java/com/example/manotien/myapplication/Main2Activity.java
@@ -2,6 +2,7 @@ package com.example.manotien.myapplication;
 
 import android.app.DatePickerDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
@@ -10,6 +11,7 @@ import android.os.Bundle;
 import android.app.DatePickerDialog.OnDateSetListener;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.InputType;
@@ -23,6 +25,7 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -46,9 +49,9 @@ public class Main2Activity extends AppCompatActivity {
     Context context = this;
     SQLiteDatabase sqLiteDatabase;
     Cursor cursor;
-
-    private final String[] list =  {
-            "AABC DDD", "AAAB","AACB DDD","ABCD CCC","ABDS","ABCS"};
+    ArrayList cocol_item = new ArrayList();
+    private final String[] list = {
+            "AABC DDD", "AAAB", "AACB DDD", "ABCD CCC", "ABDS", "ABCS"};
 
     private final String[] protect_item = new String[]{"Test", "BBBB", "AAAA"};
 
@@ -82,20 +85,20 @@ public class Main2Activity extends AppCompatActivity {
                 newDate.set(year, monthOfYear, dayOfMonth);
                 fromDateEtxt.setText(dateFormatter.format(newDate.getTime()));
             }
-        },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+        }, newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
 
 //spinner
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, protect_item);
-        Spinner province = (Spinner)findViewById(R.id.protectedspin);
+        Spinner province = (Spinner) findViewById(R.id.protectedspin);
         province.setAdapter(adapter);
 
         //Autocomplete Habitat
         //String [] habitat={};
-        List<String> habitatList = new ArrayList<>() ;
+        List<String> habitatList = new ArrayList<>();
         dbOperator = new DbOperator(getApplicationContext());
         sqLiteDatabase = dbOperator.getReadableDatabase();
         cursor = dbOperator.GetHabitatList(sqLiteDatabase);
-        if(cursor.moveToFirst()){
+        if (cursor.moveToFirst()) {
             do {
                 habitatList.add(cursor.getString(0));
             } while (cursor.moveToNext());
@@ -103,45 +106,93 @@ public class Main2Activity extends AppCompatActivity {
             cursor.close();
         }
 
-        AutoCompleteTextView habitat = (AutoCompleteTextView)findViewById(R.id.habitatedit);
-        ArrayAdapter habit_array = new ArrayAdapter(this,android.R.layout.simple_list_item_1,habitatList);
+        AutoCompleteTextView habitat = (AutoCompleteTextView) findViewById(R.id.habitatedit);
+        ArrayAdapter habit_array = new ArrayAdapter(this, android.R.layout.simple_list_item_1, habitatList);
         habitat.setAdapter(habit_array);
 
+//click button to show co-collector dialog
+        final EditText cobut = (EditText) findViewById(R.id.cocollecbut);
+        cobut.setInputType(InputType.TYPE_NULL);
+        cobut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String[] items = {" Easy ", " Medium ", " Hard ", " Very Hard "};
+// arraylist to keep the selected items
+                final ArrayList seletedItems = new ArrayList();
+
+                AlertDialog dialog = new AlertDialog.Builder(Main2Activity.this)
+                        .setTitle("Select The Difficulty Level")
+                        .setMultiChoiceItems(items, null, new DialogInterface.OnMultiChoiceClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int indexSelected, boolean isChecked) {
+                                if (isChecked) {
+                                    // If the user checked the item, add it to the selected items
+                                    seletedItems.add(indexSelected);
+                                } else if (seletedItems.contains(indexSelected)) {
+                                    // Else, if the item is already in the array, remove it
+                                    seletedItems.remove(Integer.valueOf(indexSelected));
+                                }
+                            }
+                        }).setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int id) {
+                                String cocollector="";
+                                for (Object item : seletedItems) {
+                                    //cocol_item.add();
+                                    cocollector=cocollector+items[Integer.valueOf((Integer) item)]+",";
+                                }
+                                cocollector = cocollector.substring(0, cocollector.length()-1);
+                                if(cocollector.length()!=0)
+                                    cobut.setText(cocollector);
+                            }
+                        }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int id) {
+
+                            }
+                        }).create();
+                dialog.show();
+
+            }
+        });
 
 //submit data
-        Button button = (Button)findViewById(R.id.nextbut);
+        Button button = (Button) findViewById(R.id.nextbut);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                String[] date = fromDateEtxt.getText().toString().split("-");
-                String day = date[0];
-                String month = date[1];
-                String year = date [2];
-
+                String day = "";
+                String month = "";
+                String year = "";
+                if(fromDateEtxt.getText().toString().length()!=0) {
+                    String[] date = fromDateEtxt.getText().toString().split("-");
+                    day = date[0];
+                    month = date[1];
+                    year = date[2];
+                }
                 EditText placename = (EditText) findViewById(R.id.placenameedit);
                 Spinner protect = (Spinner) findViewById(R.id.protectedspin);
                 EditText locality = (EditText) findViewById(R.id.localnoteedit);
-                AutoCompleteTextView habitat = (AutoCompleteTextView)findViewById(R.id.habitatedit);
-                EditText country = (EditText)findViewById(R.id.countryedit);
-                EditText region = (EditText)findViewById(R.id.regionedit);
-                EditText province = (EditText)findViewById(R.id.provinceedit);
-                EditText district = (EditText)findViewById(R.id.districtedit);
-                EditText subdistrict = (EditText)findViewById(R.id.subdisedit);
-                EditText collector = (EditText)findViewById(R.id.collectoredit);
+                AutoCompleteTextView habitat = (AutoCompleteTextView) findViewById(R.id.habitatedit);
+                EditText country = (EditText) findViewById(R.id.countryedit);
+                EditText region = (EditText) findViewById(R.id.regionedit);
+                EditText province = (EditText) findViewById(R.id.provinceedit);
+                EditText district = (EditText) findViewById(R.id.districtedit);
+                EditText subdistrict = (EditText) findViewById(R.id.subdisedit);
+                EditText collector = (EditText) findViewById(R.id.collectoredit);
                 String cocollector = "Tien, AAA";
                 dbOperator = new DbOperator(context);
                 sqLiteDatabase = dbOperator.getWritableDatabase();
                 dbOperator.AddLocationInformation(placename.getText().toString(), protect.getSelectedItem().toString(), locality.getText().toString(), habitat.getText().toString(), country.getText().toString(),
-                        region.getText().toString(), province.getText().toString(), district.getText().toString(), subdistrict.getText().toString(), collector.getText().toString(), cocollector, date[0], date[1], date[2], sqLiteDatabase);
+                        region.getText().toString(), province.getText().toString(), district.getText().toString(), subdistrict.getText().toString(), collector.getText().toString(), cocollector, day, month, year, sqLiteDatabase);
                 dbOperator.close();
 
-                SharedPreferences sp = getSharedPreferences("place_date",Context.MODE_PRIVATE);
+                SharedPreferences sp = getSharedPreferences("place_date", Context.MODE_PRIVATE);
                 SharedPreferences.Editor editor = sp.edit();
                 editor.putString("place_name", placename.getText().toString());
-                editor.putString("day",day );
-                editor.putString("month",month);
-                editor.putString("year",year);
+                editor.putString("day", day);
+                editor.putString("month", month);
+                editor.putString("year", year);
                 editor.commit();
                 Intent intent = new Intent(Main2Activity.this, Survey_Main.class);
                 startActivity(intent);
@@ -150,3 +201,4 @@ public class Main2Activity extends AppCompatActivity {
 
     }
 }
+
--- app/src/main/java/com/example/manotien/myapplication/MainMenuActivity.java
+++ app/src/main/java/com/example/manotien/myapplication/MainMenuActivity.java
@@ -94,53 +94,8 @@ public class MainMenuActivity extends AppCompatActivity
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                startActivity(new Intent(MainMenuActivity.this, Export_Data.class));
 
-                File dbFile = getDatabasePath("FLORA");
-                DbOperator dbhelper = new DbOperator(getApplicationContext());
-                File exportDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS), "Flora");
-                if (!exportDir.exists()) {
-                    exportDir.mkdirs();
-                }
-
-                File file = new File(exportDir, "csvname.csv");
-
-                try {
-                    file.createNewFile();
-                    CSVWriter csvWrite = new CSVWriter(new FileWriter(file));
-                    SQLiteDatabase db = dbhelper.getReadableDatabase();
-                    String query = "SELECT LOCATION_TABLE.dubs,LOCATION_TABLE.barcode,LOCATION_TABLE.accession,LOCATION_TABLE.collector,LOCATION_TABLE.addcoll,LOCATION_TABLE.prefix,LOCATION_TABLE.number,LOCATION_TABLE.suffix,LOCATION_TABLE.collection_day,LOCATION_TABLE.collection_month,LOCATION_TABLE.collection_year,"
-                            +"FLORA_TABLE.family,FLORA_TABLE.genus,FLORA_TABLE.cf,FLORA_TABLE.sp1,FLORA_TABLE.author1,FLORA_TABLE.rank1,FLORA_TABLE.sp2,FLORA_TABLE.author2,FLORA_TABLE.rank2,FLORA_TABLE.sp3,FLORA_TABLE.author3,FLORA_TABLE.plant_description,FLORA_TABLE.phenology,FLORA_TABLE.detby,FLORA_TABLE.detdd,FLORA_TABLE.detmm,FLORA_TABLE.detyy,FLORA_TABLE.detnotes,"
-                            +"LOCATION_TABLE.country,LOCATION_TABLE.florareg,LOCATION_TABLE.bkfareacod,LOCATION_TABLE.majorarea,LOCATION_TABLE.minorarea,LOCATION_TABLE.tambon,LOCATION_TABLE.protected,LOCATION_TABLE.gazetteer,LOCATION_TABLE.locality_notes,LOCATION_TABLE.habitat,"
-                            +"FLORA_TABLE.cultivated,FLORA_TABLE.cultnotes,FLORA_TABLE.notes,FLORA_TABLE.lat,FLORA_TABLE.ns,FLORA_TABLE.long,FLORA_TABLE.ew,FLORA_TABLE.alt,FLORA_TABLE.altmax,FLORA_TABLE.altnote,FLORA_TABLE.vernacular,FLORA_TABLE.language "
-                            +"FROM LOCATION_TABLE INNER JOIN FLORA_TABLE ON FLORA_TABLE.location_id=LOCATION_TABLE.id;";
-
-                    String test ="SELECT * FROM genus_table";
-                    Cursor curCSV = db.rawQuery(test, null);
-                    csvWrite.writeNext(curCSV.getColumnNames());
-
-                    while (curCSV.moveToNext()) {
-                        String arrStr[] = {curCSV.getString(0), curCSV.getString(1),curCSV.getString(2)};
-                        /*
-                       String arrStr[] = {curCSV.getString(0), curCSV.getString(1), curCSV.getString(2), curCSV.getString(3),
-                                curCSV.getString(4), curCSV.getString(5), curCSV.getString(6), curCSV.getString(7), curCSV.getString(8),
-                                curCSV.getString(9), curCSV.getString(10), curCSV.getString(11), curCSV.getString(12), curCSV.getString(13),
-                                curCSV.getString(14), curCSV.getString(15), curCSV.getString(16), curCSV.getString(17), curCSV.getString(18),
-                                curCSV.getString(19), curCSV.getString(20), curCSV.getString(21), curCSV.getString(22), curCSV.getString(23),
-                                curCSV.getString(24), curCSV.getString(25), curCSV.getString(26), curCSV.getString(27), curCSV.getString(28),
-                                curCSV.getString(29), curCSV.getString(30), curCSV.getString(31), curCSV.getString(32), curCSV.getString(33),
-                                curCSV.getString(34), curCSV.getString(35), curCSV.getString(36), curCSV.getString(37), curCSV.getString(38),
-                                curCSV.getString(39), curCSV.getString(40), curCSV.getString(41), curCSV.getString(42), curCSV.getString(43),
-                                curCSV.getString(44), curCSV.getString(45), curCSV.getString(46), curCSV.getString(47), curCSV.getString(48),
-                                curCSV.getString(49), curCSV.getString(50)};
-*/
-                        csvWrite.writeNext(arrStr);
-                    }
-                    csvWrite.close();
-                    curCSV.close();
-                    Toast.makeText(getApplicationContext(), "Export Success", Toast.LENGTH_LONG).show();
-                } catch (Exception sqlEx) {
-                    Log.d("MainActivity", sqlEx.getMessage(), sqlEx);
-                }
             }
         });
     }
